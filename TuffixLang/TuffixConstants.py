d5bc285660e612edba9338090d1589576c47a536
JaredDyreson <jared.dyreson@gmail.com>
2020-06-06 14:19:48 -0700

    moving away from Ansible playbooks and migrating to python-apt

--- TuffixLang/TuffixConstants.py
+++ TuffixLang/TuffixConstants.py
@@ -4,7 +4,6 @@ AUTHOR: Jared Dyreson
 INSTITUTION: California State University Fullerton
 """
 
-TuffixCacheDir = "/home/jared/Projects/SpotifyProject/TuffixLang/cache/"
-# TuffixAnsibleTargetsDir = "/etc/tuffix/playbooks"
-TuffixAnsibleTargetDir = "/home/jared/Projects/SpotifyProject/TuffixLang/AnsibleTargets/"
+TuffixCacheDir = "/tmp"
+TuffixAnsibleTargetDir = "/tmp"
 TuffixTargetInstalledManifest = "{}/.installed_targets".format(TuffixAnsibleTargetDir)
